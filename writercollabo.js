243ee8401b0e952a022b1cf52f117f4e6ec8f758
Anthony <tony.anaedu@gmail.com>
2015-12-16 11:04:24 +0100

    Add Collection and Startup data

--- writercollabo.js
+++ writercollabo.js
@@ -1,9 +1,13 @@
+//Allows the sharejs package to access the document
+this.Documents = new Mongo.Collection("documents");
 if (Meteor.isClient) {
   
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+    if(!Documents.findOne()){
+      Documents.insert({title: "New document"});
+    }
   });
 }
